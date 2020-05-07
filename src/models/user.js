c8a8e38e90839fbd91f1621e166cca3e76c1d515
Anuj Singh <anujs787@gmail.com>
2020-05-07 13:56:02 +0530

    Added basic authentication

--- src/models/user.js
+++ src/models/user.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
-const User = mongoose.model('user', {
+const userSchema = new mongoose.Schema({
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     }, 
     email: {
         type: String,
-        requried: true,
+        required: true,
+        unique: true,
         trim: true,
         lowercase: true,
         validate(value) {
@@ -37,5 +41,33 @@ const User = mongoose.model('user', {
         }
     }
 })
+
+userSchema.pre('save', async function(next) {
+    const user = this
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new Error('Unable to login')
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if (!match) {
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
+const User = mongoose.model('user', userSchema)
 
 module.exports = User
